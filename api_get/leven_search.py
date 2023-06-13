d953f735adc2c3aa36992f03893358c965adbdbf
heko <91366057+hkohko@users.noreply.github.com>
2023-06-13 09:49:35 +0700

    implemented custom Levenshtein ratios

---
+++ api_get/leven_search.py
@@ -0,0 +1,15 @@
+from Levenshtein import ratio
+
+async def fsearch(entry, choices):
+    cutoff = 0.55
+    candidates = {}
+
+    for i in choices:
+        score = ratio(entry, i)
+        if score > cutoff:
+            candidates[i] = score
+
+    try:
+        return max(candidates, key=candidates.get)
+    except ValueError:
+        pass
