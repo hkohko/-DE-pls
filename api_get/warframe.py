4e9fb6c5a84fa530486cb844c2e00fee2316fb96
heko <91366057+hkohko@users.noreply.github.com>
2023-06-13 09:57:58 +0700

    Revert "Revert "close file only after command's finished""
    This reverts commit 654954342d347e439d4c80a40948a6e9515f57bc.

--- api_get/warframe.py
+++ api_get/warframe.py
@@ -1,14 +1,11 @@
 import aiohttp
 import asyncio
 import discord
-from fuzzywuzzy import process
 from bs4 import BeautifulSoup
+import api_get.leven_search as leven
 
 escape = f'\n'
 
-def del_db():
-  global wf_item, wf_mod, wf_frames, wf_wep, _names
-  del wf_item, wf_mod, wf_frames, wf_wep, _names
 
 async def get_db(URL):
     async with aiohttp.ClientSession() as session:
@@ -16,52 +13,66 @@ async def get_db(URL):
             response_json = await response.json()
     return response_json
 
-async def init_db():
-    global wf_item, wf_mod, wf_frames, wf_wep, _names
-    URL = f'https://api.warframestat.us/items/'
-    URL_mod = f'https://api.warframestat.us/mods/'
-    URL_frames = f'https://api.warframestat.us/warframes/'
-    URL_wep = f'https://api.warframestat.us/weapons/'
 
-    r = get_db(URL)
-    r_mod = get_db(URL_mod)
-    r_frames = get_db(URL_frames)
-    r_wep = get_db(URL_wep)
+class WF_init:
+    async def init_db(self):
+        URL = f'https://api.warframestat.us/items/'
+        URL_mod = f'https://api.warframestat.us/mods/'
+        URL_frames = f'https://api.warframestat.us/warframes/'
+        URL_wep = f'https://api.warframestat.us/weapons/'
 
-    r, r_mod, r_frames, r_wep = await asyncio.gather(r, r_mod, r_frames, r_wep)
+        r = get_db(URL)
+        r_mod = get_db(URL_mod)
+        r_frames = get_db(URL_frames)
+        r_wep = get_db(URL_wep)
 
-    wf_item = r
-    wf_mod = r_mod
-    wf_frames = r_frames
-    wf_wep = r_wep
-    _names = [wf_item[i]['name'] for i, j in enumerate(wf_item)]
+        r, r_mod, r_frames, r_wep = await asyncio.gather(r, r_mod, r_frames, r_wep)
 
-async def weapons_db():
-    global weapondb
-    weapondb = {}
-    for i, _ in enumerate(wf_wep):
-        name = wf_wep[i]["name"]
-        weapondb[name] = i
+        self.wf_item = r
+        self.wf_mod = r_mod
+        self.wf_frames = r_frames
+        self.wf_wep = r_wep
+        self._names = [self.wf_item[i]['name'] for i, _ in enumerate(self.wf_item)]
 
-async def frames_db():
-    global framedb
-    framedb = {}
-    for i, _ in enumerate(wf_frames):
-        name = wf_frames[i]["name"]
-        framedb[name] = i
+    async def del_db(self):
+        del self.wf_item, self.wf_mod, self.wf_frames, self.wf_wep, self._names
 
-async def mod_db():
-    global moddb
-    moddb = {}
-    for i, _ in enumerate(wf_mod):
-        name = wf_mod[i]["name"]
-        moddb[name] = i
+class Lookup:
+    async def weapons_db(self):
+        self.weapondb = {}
+        for i, _ in enumerate(wf_init.wf_wep):
+            name = wf_init.wf_wep[i]["name"]
+            self.weapondb[name] = i
 
-async def fsearch(entry):
-    a = process.extractOne(entry, _names)
-    return a[0]
 
-async def wfmod(img):
+    async def frames_db(self):
+        self.framedb = {}
+        for i, _ in enumerate(wf_init.wf_frames):
+            name = wf_init.wf_frames[i]["name"]
+            self.framedb[name] = i
+
+
+    async def mod_db(self):
+        self.moddb = {}
+        for i, _ in enumerate(wf_init.wf_mod):
+            name = wf_init.wf_mod[i]["name"]
+            self.moddb[name] = i
+
+
+wf_init = WF_init()    
+lookup = Lookup()
+
+
+async def initialize():
+    await wf_init.init_db()
+
+
+async def clear_db():
+    await wf_init.del_db()
+
+
+async def wiki_image(img):
+    
     webpage = f'https://warframe.fandom.com/wiki/{img}'.replace(" ", "_")
     async def get_image():
         async with aiohttp.ClientSession() as session:
@@ -77,35 +88,41 @@ async def wfmod(img):
         class_=
         'portable-infobox pi-background pi-border-color pi-theme-wikia pi-layout-default'
     )
+
     img = sidebar.find('a', class_='image image-thumbnail').get('href')
+    
     return (img)
 
+
 async def mod(var):
-    await mod_db()
+
+    await lookup.mod_db()
+
     display = []
-    entry_mod = await fsearch(var)
-    num = moddb[entry_mod]
+
+    entry_mod = await leven.fsearch(var, wf_init._names)
+    num = lookup.moddb[entry_mod]
     embed = discord.Embed(
         colour=discord.Colour.dark_purple(),
         title=f"{entry_mod}",
-        url=f"{wf_mod[num]['wikiaUrl']}"
+        url=f"{wf_init.wf_mod[num]['wikiaUrl']}"
     )
-    try:
-        embed.set_thumbnail(url=f"{wf_mod[num]['wikiaThumbnail']}")
-    except KeyError:
-        embed.set_thumbnail(url=f"{await wfmod(entry_mod)}")
 
+    try:
+        embed.set_thumbnail(url=f"{wf_init.wf_mod[num]['wikiaThumbnail']}")
+    except KeyError:
+        embed.set_thumbnail(url=f"{await wiki_image(entry_mod)}")
 
     enum = []
 
     try:
-        if wf_mod[num]['name'] == entry_mod:
-            if wf_mod[num].get('drops') == None:
+        if wf_init.wf_mod[num]['name'] == entry_mod:
+            if wf_init.wf_mod[num].get('drops') == None:
                 embed.add_field(name="Showing the first 10 locations:", value=f"```Drop table data unavailable```")
                 return embed
             else:
                 display.append('```')
-                for y, x in enumerate(wf_mod[num]['drops']):
+                for y, x in enumerate(wf_init.wf_mod[num]['drops']):
                     display.append(f"- {x['location']}")
                     enum.append(y)
                     if len(enum) == 10:
@@ -117,25 +134,27 @@ async def mod(var):
     embed.add_field(name="Showing the first 10 locations:", value=f"{escape.join(display)}")
     return embed
 
+
 async def weapon(var):
-    await weapons_db()
-    entry = await fsearch(var)
-    num = weapondb[entry]
+    await lookup.weapons_db()
+    entry = await leven.fsearch(var, wf_init._names)
+    num = lookup.weapondb[entry]
     embed = discord.Embed(
     colour=discord.Colour.dark_purple(),
     title=f"{entry}",
-    url=f"{wf_wep[num]['wikiaUrl']}",
-)
+    url=f"{wf_init.wf_wep[num]['wikiaUrl']}",
+    )
+
     try:
-        embed.set_thumbnail(url=f"{wf_wep[num]['wikiaThumbnail']}")
+        embed.set_thumbnail(url=f"{wf_init.wf_wep[num]['wikiaThumbnail']}")
     except KeyError:
-        embed.set_thumbnail(url=f"{await wfmod(entry)}")
+        embed.set_thumbnail(url=f"{await wiki_image(entry)}")
 
     display = []
     dmg = []
     try:
-        if wf_wep[num]['name'] == entry:
-            for j in wf_wep[num]['attacks']:
+        if wf_init.wf_wep[num]['name'] == entry:
+            for j in wf_init.wf_wep[num]['attacks']:
                 embed.add_field(name=f"{j['name']}", value=f"Crit. Chance\n{j['crit_chance']}%\nCrit. Damage\n{j['crit_mult']}x\nStat. Chance\n{j['status_chance']}%")
 
                 dmg_dict = j['damage']
@@ -156,29 +175,28 @@ async def weapon(var):
         return  embed
     return embed
 
+
 async def frame(var):
 
-    await frames_db()
+    await lookup.frames_db()
 
-    entry = await fsearch(var)
-    num = framedb[entry]
+    entry = await leven.fsearch(var, wf_init._names)
+    num = lookup.framedb[entry]
 
     embed = discord.Embed(
     colour=discord.Colour.dark_purple(),
     title=f"{entry}",
-    url=f"{wf_frames[num]['wikiaUrl']}",
-)
-    try:
-        embed.set_thumbnail(url=f"{wf_frames[num]['wikiaThumbnail']}")
-    except KeyError:
-        embed.set_thumbnail(url=f"{await wfmod(entry)}")
-    
-    try:
+    url=f"{wf_init.wf_frames[num]['wikiaUrl']}",
+    )
 
-        embed.add_field(name=f"Stats at Rank 30:", value=f"Armor - {wf_frames[num]['armor']}\nShield - {wf_frames[num]['shield']*3}\nHealth - {wf_frames[num]['health']*3}\nEnergy - {int(wf_frames[num]['power']*1.5)}")
-
+    try:
+        embed.set_thumbnail(url=f"{wf_init.wf_frames[num]['wikiaThumbnail']}")
     except KeyError:
-        
-        embed.add_field(name=f"Stats at Rank 30:", value=f"Armor - {wf_frames[num]['armor']}\nShield - {wf_frames[num]['shield']*3}\nHealth - {wf_frames[num]['health']*3}\nEnergy - {int(wf_frames[num]['power']*1.5)}")
+        embed.set_thumbnail(url=f"{await wiki_image(entry)}")
+
+    try:
+        embed.add_field(name=f"Stats at Rank 30:", value=f"Armor - {wf_init.wf_frames[num]['armor']}\nShield - {wf_init.wf_frames[num]['shield']*3}\nHealth - {wf_init.wf_frames[num]['health']*3}\nEnergy - {int(wf_init.wf_frames[num]['power']*1.5)}")
+    except KeyError: 
+        embed.add_field(name=f"Stats at Rank 30:", value=f"Armor - {wf_init.wf_frames[num]['armor']}\nShield - {wf_init.wf_frames[num]['shield']*3}\nHealth - {wf_init.wf_frames[num]['health']*3}\nEnergy - {int(wf_init.wf_frames[num]['power']*1.5)}")
 
     return embed
